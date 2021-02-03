9baa6db12d71d9c5be9ab38aa63491a7c12e6937
Cat Core <arthur.bambou@gmail.com>
2021-02-03 14:31:11 +0100

    echo

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+echo "======APT Updates======"
 sudo apt update
 sudo apt list --upgradable
 sudo apt upgrade
 
+echo "====Flatpak Updates===="
 flatpak update
 
+echo "=====Other Updates====="
 python3 <(curl -s https://raw.githubusercontent.com/arthurbambou/my-scripts/main/update-not-ppa-deb.py) update
+
+echo "==========Done========="
